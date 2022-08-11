b2c7e206ac6c5ba51cecc0ed2eed8bde36b6865f
Khalil Attalla Firdia <khalilattalla2209@gmail.com>
2022-08-11 10:13:14 +0700

    make folder server to connect mysql

---
+++ server/server.go
@@ -0,0 +1,16 @@
+package server
+
+import (
+	"database/sql"
+	_ "mysql-master"
+)
+
+func Koneksi() (*sql.DB, error) {
+	db, err := sql.Open("mysql", "root:@TCP(localhost)/db_pizza")
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
